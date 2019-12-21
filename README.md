bdb9fb366986b4be88d1656beb9bd24250794a73
Marti Raudsepp <marti@juffo.org>
2019-12-21 15:43:45 +0200

    Add project files

---
+++ README.md
@@ -0,0 +1,19 @@
+<a href="http://mypy-lang.org/">
+<img src="http://mypy-lang.org/static/mypy_light.svg" alt="mypy logo" width="300px"/>
+</a>
+
+# Typing stubs for drf-yasg library
+
+PEP 484 type stubs for Mypy and PyCharm.
+
+This package contains type stubs to provide more precise static types and
+type inference for drf-yasg, the OpenAPI/Swagger generator for Django REST
+Framework.
+
+## Installation
+
+```bash
+pip install drf-yasg-stubs
+```
+
+That's all, run Mypy and it should be able to pick up these stubs.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="drf-yasg-stubs",
+    version="0.0.1",
+    description="Typing stubs for drf-yasg library (PEP 484 stubs for Mypy and PyCharm)",
+    license="MIT",
+    url="https://github.com/intgr/drf-yasg-stubs",
+    author="Marti Raudsepp",
+    author_email="marti@juffo.org",
+    python_requires=">=3.6",
+    install_requires=["drf-yasg"],
+    packages=["drf_yasg-stubs"],
+    package_data={"drf_yasg-stubs": ["*.pyi"],},
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
