7ac7b04abf086c8fe37eac9f971c050fd7a4f6a4
Marti Raudsepp <marti@juffo.org>
2020-01-13 01:08:09 +0200

    Release version 0.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def find_stub_files(name: str) -> List[str]:
 
 setup(
     name="drf-yasg-stubs",
-    version="0.0.dev1",
+    version="0.1.0",
     description="Typing stubs for drf-yasg library (PEP 484 stubs for Mypy and PyCharm)",
     license="MIT",
     url="https://github.com/intgr/drf-yasg-stubs",
