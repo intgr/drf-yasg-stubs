d4c928da7ced52c751e7760dd644f49aacf9335d
Marti Raudsepp <marti@juffo.org>
2021-01-15 12:41:07 +0200

    Add appropriate re-exports to inspectors/__init__.py (#13)

--- drf_yasg-stubs/inspectors/__init__.pyi
+++ drf_yasg-stubs/inspectors/__init__.pyi
@@ -1,26 +1,29 @@
 from .base import (
-    BaseInspector,
-    FieldInspector,
-    FilterInspector,
-    NotHandled,
-    PaginatorInspector,
-    SerializerInspector,
-    ViewInspector,
+    BaseInspector as BaseInspector,
+    FieldInspector as FieldInspector,
+    FilterInspector as FilterInspector,
+    NotHandled as NotHandled,
+    PaginatorInspector as PaginatorInspector,
+    SerializerInspector as SerializerInspector,
+    ViewInspector as ViewInspector,
 )
 from .field import (
-    CamelCaseJSONFilter,
-    ChoiceFieldInspector,
-    DictFieldInspector,
-    FileFieldInspector,
-    HiddenFieldInspector,
-    InlineSerializerInspector,
-    JSONFieldInspector,
-    RecursiveFieldInspector,
-    ReferencingSerializerInspector,
-    RelatedFieldInspector,
-    SerializerMethodFieldInspector,
-    SimpleFieldInspector,
-    StringDefaultFieldInspector,
+    CamelCaseJSONFilter as CamelCaseJSONFilter,
+    ChoiceFieldInspector as ChoiceFieldInspector,
+    DictFieldInspector as DictFieldInspector,
+    FileFieldInspector as FileFieldInspector,
+    HiddenFieldInspector as HiddenFieldInspector,
+    InlineSerializerInspector as InlineSerializerInspector,
+    JSONFieldInspector as JSONFieldInspector,
+    RecursiveFieldInspector as RecursiveFieldInspector,
+    ReferencingSerializerInspector as ReferencingSerializerInspector,
+    RelatedFieldInspector as RelatedFieldInspector,
+    SerializerMethodFieldInspector as SerializerMethodFieldInspector,
+    SimpleFieldInspector as SimpleFieldInspector,
+    StringDefaultFieldInspector as StringDefaultFieldInspector,
 )
-from .query import CoreAPICompatInspector, DjangoRestResponsePagination
-from .view import SwaggerAutoSchema
+from .query import (
+    CoreAPICompatInspector as CoreAPICompatInspector,
+    DjangoRestResponsePagination as DjangoRestResponsePagination,
+)
+from .view import SwaggerAutoSchema as SwaggerAutoSchema
